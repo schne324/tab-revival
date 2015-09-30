c032f28d356599fe80ca463c87dbd75471e1c719
Harris Schneiderman <schne324@gmail.com>
2015-09-30 19:02:24 -0400

    Zipsssssssssssss

--- test/stairway-to-heaven.js
+++ test/stairway-to-heaven.js
@@ -2,7 +2,7 @@
 var parser = require('../scripts/parser.js');
 var fs = require('fs-extra');
 var assert = require('better-assert');
-var tab = fs.readFileSync('test/stairway-to-heaven.txt', 'utf-8');
+var tab = fs.readFileSync('test/tabs/stairway-to-heaven.txt', 'utf-8');
 
 
 describe('tab parser', function () {
